a0f1943e387f94aa190c01a23db8a29da9e12a0f
UribesEffect <38766156+UribesEffect@users.noreply.github.com>
2018-11-20 09:32:55 -0500

    Update primer_paso.m

--- primer_paso.m
+++ primer_paso.m
@@ -7,10 +7,11 @@ p = 5; % de 1 a 10000
 j = Senal_ruido(1:10000-p,1); % 
 s = Senal_ruido(10000-p:9999,1); % 
 AH = hankel(j,s); % Matriz de Hankel de n-p filas y p columnas 
-Y = c(p+1:10000,1); % Y es la señal oroginal
-size(j);% size es para mirar el tamaño de eso
-size(AH);% size es para mirar el tamaño de eso
+Y = c(p+1:10000,1); % Y es la seÃ±al oroginal
+size(j);% size es para mirar el tamaÃ±o de eso
+size(AH);% size es para mirar el tamaÃ±o de eso
 X=AH\Y % X es el coeficiente del filtro
 Y_e = AH * X; % Ni idea
 e = mean((Y_e-Y).^2) % Ni idea
 plot(X)%Grafica
+hold on
